9b7ae2f64ed971ac2fe92b234dbf36f9f12ff82c
N0mena <alida.rajaonarivelo@gmail.com>
2025-11-20 20:52:52 +0300

    feat: nothing yet 2.0

---
+++ evaluation/src/test/java/notes/MarkTest.java
@@ -0,0 +1,26 @@
+package notes;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MarkTest {
+    private Student Rondro;
+    private Mark Mark1;
+    private Exam finalExam;
+    private Course PROG;
+
+@BeforeEach
+public void setUp(){
+    Rondro = new Student(1,'');
+}
+
+
+
+
+}
