62bca556f2d7a49e56d3b97b4a59a5ad9403bfdf
N0mena <alida.rajaonarivelo@gmail.com>
2025-11-20 23:36:27 +0300

    feat: not yet done

--- evaluation/src/main/java/notes/Student.java
+++ evaluation/src/main/java/notes/Student.java
@@ -9,14 +9,14 @@ import lombok.*;
 
 public class Student extends User{
     private final Group group;
-    private final List<Tutor> tutor;
-    private final List<Course> course;
+    private final Tutor tutor;
+    private final Course course;
 
      Student(String id, String first_name, String last_name, LocalDate date_of_birth, String email, long phone_number, Group group,Tutor tutor, Course course){
         super(id,first_name,last_name,date_of_birth,email,phone_number);
         this.group=group;
-        this.course=new ArrayList<>();
-        this.tutor=new ArrayList<>();
+        this.course=course;
+        this.tutor=tutor;
     }
 
 }
--- evaluation/src/test/java/notes/MarkTest.java
+++ evaluation/src/test/java/notes/MarkTest.java
@@ -11,15 +11,17 @@ import static org.junit.Assert.*;
 
 public class MarkTest {
     private Student Rondro;
+    private Tutor tutorRondro;
+    private Course course1;
     private Mark Mark1;
     private Exam finalExam;
     private Course PROG;
 
 @BeforeEach
-public void setUp(){
-    Rondro = new Student("1" ,"Rakoto","Rondro", "2004-12-31", "rondro@gmail.com","0345678762", "K2", );
+public void setUp() {
+    Rondro = new Student("1", "Rakoto", "Rondro", "2004-12-31", "rondro@gmail.com", "0345678762", "K2", tutorRondro, );
+    tutorRondro = new Tutor("1", "Rakoto","Rafe","1980-01-12","rafe@gmail.com","0327689840", );
 }
-
 
 
 
